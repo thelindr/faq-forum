986d2aabc8c999e9d5b7bd24f8dceae4cbd224da
Therese Lindroth <31589557+thelindr@users.noreply.github.com>
2018-01-16 15:48:19 +0100

    Revert "Question Api"
    This reverts commit 1872efe79f09102039ac97a275c016262b656b6d.

--- frontend/src/components/formuser/currectFAQ.js
+++ frontend/src/components/formuser/currectFAQ.js
@@ -10,28 +10,30 @@ export default [
     id: "2",
     category: "OP-1",
     question: "how do i update the OP-1 firmware?",
-    answer: `
-      * turn OP-1 off.
-      * disconnect the usb cable.
-      * wait three seconds.
-      * press and hold the COM key on the OP-1 while turning the power on. this will take you to the TE-BOOT screen.
-      * press T1 or 1/IN on OP-1 to select upload firmware.
-      * connect OP-1 to computer using the usb cable. OP-1 will appear as a removable disk on your computer.
-      * copy the firmware file to the root of the removable disk.
-      * eject the removable disk.
-      * press the COM button on OP-1.
-
-      note: if the OS revision number at OP-1 startup is same or greater than the currently available download version, then you should not update.
-
-      the latest firmware can be found here:[teenage.engineering/downloads#os_updates]
-      `,
+    answer: {
+      first: "turn OP-1 off.",
+      second: "disconnect the usb cable.",
+      third: "wait three seconds.",
+      fourth: "press and hold the COM key on the OP-1 while turning the power on. this will take you to the TE-BOOT screen.",
+      fifth: "press T1 or 1/IN on OP-1 to select upload firmware.",
+      sixth: "connect OP-1 to computer using the usb cable. OP-1 will appear as a removable disk on your computer.",
+      seventh: "copy the firmware file to the root of the removable disk.",
+      eight: "eject the removable disk.",
+      nineth: "press the COM button on OP-1."
+    },
+    comments: {
+      first: "note: if the OS revision number at OP-1 startup is same or greater than the currently available download version, then you should not update.",
+      second: "the latest firmware can be found here:"
+      {
+        url: "teenage.engineering/downloads#os_updates",
+      }
+    },
     keywords: "OP-1"
   }, {
     id: "3",
     category: "OP-1",
     question: "how long does the battery last?",
     answer: "the battery will last 8-10 hours of constant use on a full charge. in standby mode the battery will last 16 hours.",
-    comments: "",
     keywords: "OP-1"
   }, {
     id: "4",
