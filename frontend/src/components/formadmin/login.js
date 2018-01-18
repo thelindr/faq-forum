8ee943dd090f4c36c1a59dc8fb698fa58a4e4af1
nicolepehar <nicole.pehar@gmail.com>
2018-01-18 15:29:38 +0100

    route

--- frontend/src/components/formadmin/login.js
+++ frontend/src/components/formadmin/login.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom"
 
 class Login extends React.Component {
   constructor(props) {
@@ -7,7 +7,7 @@ class Login extends React.Component {
     this.state = {
       username: "",
       password: "",
-      message: ""
+      message: "",
     }
   }
 
@@ -38,6 +38,7 @@ handleSubmit = event => {
     }).then(response => {
       if (response.ok) {
         this.props.history.push("/formadmin")
+        {this.props.isHidden}
       } else {
         this.setState({ message: "unknown user" })
       }
